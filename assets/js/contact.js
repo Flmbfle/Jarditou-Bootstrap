e6cc6a2d4a3a2806f7f20109a4ddeac38dbcf425
Flmbfle <erwantotet@gmail.com>
2023-10-19 10:19:30 +0200

    Formulaire + JS

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -13,10 +13,14 @@ var contactForm = document.getElementById("formulaire");
             var prenom = document.getElementById("exampleInputPrenom").value;
             var cp = document.getElementById("exampleInputCp").value;
             var ville = document.getElementById("exampleInputVille").value;
+            var adresse = document.getElementById('exampleInputAdresse').value;
             var email = document.getElementById("exampleInputEmail").value;
-            var sexeFeminin = document.getElementById("sexeFeminin");
-            var sexeMasculin = document.getElementById("sexeMasculin");//.checked;
+            var sexeFeminin = document.getElementById("inlineRadioFeminin").checked;
+            var sexeMasculin = document.getElementById("inlineRadioMasculin").checked;            
             var conditionsAcceptees = document.getElementById("conditions").checked;
+            var dateNaissance = document.getElementById("exampleInputDate").value;
+            var question = document.getElementById("textQuestion").value;
+            var sujet = document.getElementById("demande").value;
             var isValid = true;
 
             if (nom.length < 1) {
@@ -31,10 +35,24 @@ var contactForm = document.getElementById("formulaire");
                 isValid = false;
             }
 
+            if (!sexeFeminin && !sexeMasculin) {
+                document.getElementById("sexeError").textContent = "Veuillez sélectionner votre sexe.";
+                isValid = false;
+            }
+
             if (!/^\d{5}$/.test(cp)) {
                 console.log('cp invalide')
                 document.getElementById("cpError").textContent = "Le code postal doit comporter 5 caractères numériques.";
                 isValid = false;
+            }
+
+            if (dateNaissance === "") {
+                document.getElementById("dateError").textContent = "La date de naissance est obligatoire.";
+                isValid = false;
+            }
+
+            if (adresse === "") {
+                document.getElementById("adresseError").textContent = "Veuillez entrez votre adresse."
             }
 
             if (ville.length < 1) {
@@ -43,20 +61,25 @@ var contactForm = document.getElementById("formulaire");
                 isValid = false;
             }
 
-            if (email.indexOf("@") === -1) {
+            if (email.indexOf("@.") === -1) {
                 console.log('email invalide')
                 document.getElementById("emailError").textContent = "L'adresse email doit contenir le caractère '@'.";
                 isValid = false;
             }
-            
-            // if (!sexeFeminin && !sexeMasculin) {
-            //     document.getElementById("sexeError").textContent = "Veuillez sélectionner votre sexe.";
-            //     isValid = false;
-            // }
+
+            if (sujet === "") {
+                document.getElementById("sujetError").textContent = "Veuillez selectionner un sujet.";
+                isValid = false;
+            }
+
+            if (question.trim() === "") {
+                document.getElementById("questionError").textContent = "Veuillez renseigner ce champ.";
+                isValid = false;
+            }
         
             if (!conditionsAcceptees) {
                 console.log('conditions invalide')
-                document.getElementById("conditionsError").textContent = "Vous devez accepter les conditions.";
+                document.getElementById("conditionsError").textContent = "Merci de bien vouloir accepter les conditions.";
                 isValid = false;
             }
 
